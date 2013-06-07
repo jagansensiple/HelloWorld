2aebbc4ae79cc729b8c01ef2fb9efd4117a3a297
jagankumarr <jagankumarr@java070.epro-tech.com>
2013-06-07 18:35:20 +0530

    New Method Included

---
+++ de.vogella.git.first/src/de/vogella/git/first/MethodTest.java
@@ -0,0 +1,16 @@
+package de.vogella.git.first;
+
+public class MethodTest {
+	public static void main(String[] args) {
+
+		MethodTest mt = new MethodTest();
+		mt.display();
+		
+	}
+	
+	public void display()
+	{
+		System.out.println("this is test method");
+	}
+
+}
