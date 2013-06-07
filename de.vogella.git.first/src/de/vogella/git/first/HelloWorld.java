57b0948a930369577ac271ec63f60260bdd65cb7
jagankumarr <jagankumarr@java070.epro-tech.com>
2013-06-07 19:00:37 +0530

    newly added display and greeting method.

--- de.vogella.git.first/src/de/vogella/git/first/HelloWorld.java
+++ de.vogella.git.first/src/de/vogella/git/first/HelloWorld.java
@@ -6,6 +6,17 @@ public class HelloWorld {
 
 		
 		System.out.println("  welcome to first git hub repository project ");
+		display();
+		greeting();
+	}
+	
+	static void display()
+	{
+		System.out.println("this is display method");
+	}
+	static void greeting()
+	{
+		System.out.println("this is greeting method");
 	}
 
 }
